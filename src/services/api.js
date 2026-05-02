e8412fc19f3fca83868d39924facf4913e50261a
Mosa Mosenene <mosa.mosenene@bothouniversity.com>
2026-05-02 02:57:11 +0200

    Hardcode API_URL to fix deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // services/api.js
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://lesotho-transport-payment.onrender.com/api';
+const API_URL = 'https://lesotho-transport-payment.onrender.com/api';
 
 const api = axios.create({
     baseURL: API_URL,
